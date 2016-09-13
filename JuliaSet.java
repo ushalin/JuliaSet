862057aab68311e43e925e996e2a87f27dbd8c76
ushalin <ushalinca@gmail.com>
2016-09-12 21:39:27 -0400

    Julia Set
    Initial Commit

---
+++ JuliaSet.java
@@ -0,0 +1,150 @@
+/**
+ * Displays the graphical representation of the Julia Set
+ * for a certain complex number: f(x, y) = x + iy
+ * 
+ * Finds the Basin of Attraction Set, A(infinity) = {...}, 
+ * and determines when f(x, y) has a limit of infinity
+ * (Basin of Attraction has an attractor == infinity)
+ *
+ * @author  Shalin Upadhyay
+ * @since   2016-09-12
+ */
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class JuliaSet extends JPanel {
+
+	// Object to store output screen
+	private BufferedImage canvas;
+
+	// Basic Complex Function: z = f(cRe, cIm) = cRe + cIm*i
+	private static double cRe, cIm;
+
+	// Adjusts the screen to the coordinates (moveX, moveY)
+	// to focus on a certain portion of the Julia Set
+	private static double moveX = 0, moveY = 0;
+
+	// Magnification factor into a certain portion of the Julia Set
+	private static double zoom = 1;
+
+	// Width and Height for JFrame
+	private static int width, height;
+	
+	// Number of iterations for the function f(z) to perform
+	// to determine when the Basin of Attraction occurs
+	private static int maxIterations;
+
+	public JuliaSet() {
+
+		/* ADJUSTABLE VALUES */
+		
+		// JFrame Dimensions
+		this.width = 1000;
+		this.height = 1000;
+
+		this.cRe = -0.7; // Real Value
+		this.cIm = 0.27015; // Real Coefficient for Imaginary Value
+
+		this.moveX = 0;
+		this.moveY = 0;
+
+		this.zoom = 1;
+		
+		this.maxIterations = 250;
+		
+		/* ADJUSTABLE VALUES */
+
+		this.canvas = new BufferedImage(this.width, this.height, BufferedImage.TYPE_INT_ARGB);
+
+		colorJulia(this.width, this.height, this.moveX, this.moveY, this.zoom, this.maxIterations);
+	}
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(canvas, null, null);
+	}
+
+	public Dimension getPreferredSize() {
+		return new Dimension(canvas.getWidth(), canvas.getHeight());
+	}
+	
+	public void colorJulia(int w, int h, double moveX, double moveY, double zoom, int maxIterations) {
+
+		// Color object for color at pixel (x, y)
+		Color color;
+		
+		// Integer value (RGB) for pixel (x, y)
+		int colorVal;
+		
+		// Current iteration of f(x, y)
+		int iter;
+		
+		double nRe, nIm, oRe, oIm;
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+
+				nRe = moveX + ((1.5 * (j - w / 2)) / (0.5 * zoom * w));
+				nIm = moveY + ((i - h / 2) / (0.5 * zoom * h));
+
+				for (iter = 0; iter < maxIterations; iter++) {
+					
+					oRe = nRe;
+					oIm = nIm;
+
+					nRe = oRe * oRe - oIm * oIm + cRe;
+					nIm = 2 * oRe * oIm + cIm;
+					
+					double deter = nRe * nRe + nIm * nIm;
+					
+					if (deter > 4) {
+						break;
+					}
+				}
+
+				// If the current iteration is below maxIterations, the complex value will be painted a value
+				// to represent that it reached infinity in 'iter < maxIteration' function iterations (f^iter(z))
+				colorVal = (iter < maxIterations) ? 1 : 0;
+
+				// HSB/HSV Color value for certain values
+				color = Color.getHSBColor(iter % 256, 255, iter * colorVal);
+
+				// Changes the color of the pixel at location (x, y)
+				canvas.setRGB(j, i, color.getRGB());
+			}
+		}
+
+		// Repaints the canvas with colors for Julia Set
+		repaint();
+	}
+
+	public static void main(String[] args) {
+
+		// Object for JuliaSet class
+		JuliaSet js = new JuliaSet();
+
+		JFrame frame = new JFrame("Julia Set: " + cRe + " + " + cIm + "i");
+
+		// Adds the drawn Julia Set to the JFrame
+		frame.add(js);
+
+		// Makes the output window to fit the preferred size
+		frame.pack();
+
+		// Makes the JFrame visible
+		frame.setVisible(true);
+
+		// Prevents JFrame window from being resized
+		frame.setResizable(false);
+
+		// Ends the program when output window is closed
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
